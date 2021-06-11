dc8d35fe1e9e07748d299dd2ac32c2f9a30a740d
Thulam Tran <ttran125@uottawa.ca>
2021-06-11 13:14:10 -0400

    New Project: Connect 4; initialized board

---
+++ mini projects/connect4/connect4.py
@@ -0,0 +1,6 @@
+PLACEHOLDER="#"
+board=[]
+for i in range(6):
+    board+=[[PLACEHOLDER,PLACEHOLDER,PLACEHOLDER,PLACEHOLDER,PLACEHOLDER,PLACEHOLDER,PLACEHOLDER]]
+for i in range(len(board)):
+    print(board[i])
