bd294a035f6fcbdfd94145c3a5370ba8d9683f41
Thulam Tran <ttran125@uottawa.ca>
2021-06-23 15:23:28 -0400

    Added board, x and o assets.

--- mini projects/tictactoe/main.py
+++ mini projects/tictactoe/main.py
@@ -1,29 +1,53 @@
 import random
+import os
 import pygame
-from tictactoe import *
+pygame.init()
+
 
 #tictactoe()
 #play()
 
-pygame.init()
+#constants
+WIDTH, HEIGHT = 500, 500
+GRAY=(128, 128, 128)
+FPS=60
+
+#FIX PATHS LATER TO NOT USE ABSOLUTE 
+BOARD_IMAGE=pygame.image.load("C:/Users/thula/dev/projects/projects/mini projects/tictactoe/Assets/board_image.png") #os.path.join('Assets', 'board_image.png')
+BOARD_IMAGE=pygame.transform.scale(BOARD_IMAGE,(500,500))
+CIRCLE=pygame.image.load("C:/Users/thula/dev/projects/projects/mini projects/tictactoe/Assets/circle.png") #os.path.join('Assets', 'circle.png')
+CIRCLE=pygame.transform.scale(CIRCLE,(100,100))
+CROSS=pygame.image.load("C:/Users/thula/dev/projects/projects/mini projects/tictactoe/Assets/cross.png") #os.path.join('Assets', 'cross.png')
+CROSS=pygame.transform.scale(CROSS,(100,100))
 
 #Create new window of size WIDTH and HEIGHT constants, and window name
-WIDTH, HEIGHT = 500, 500
 screen=pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("TIC TAC TOE")
 
+def draw_window():
+    #Fill background color
+    screen.fill(GRAY)
+
+    #Draw board_image
+    screen.blit(BOARD_IMAGE, (0,0))
+    screen.blit(CIRCLE, (40,40))
+    screen.blit(CROSS, (200,200))
+    #update display
+    pygame.display.update()
+
 #main game loop
 def main():
+    clock=pygame.time.Clock()
     running=True
     while running:
+        clock.tick(FPS) #cap FPS
         for event in pygame.event.get():
             #Exit event when exiting window
             if event.type==pygame.QUIT:
                 running=False
         
-        screen.fill((255,255,255))
-        pygame.draw.circle(screen,(0,0,255),(250,250),75)
-        pygame.display.flip()
+        draw_window()
+
     #Exit pygame
     pygame.quit()
 
